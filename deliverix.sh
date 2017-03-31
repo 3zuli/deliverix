98898099c52aa124a499847fd31e51150d73e934
Adam Trizuljak <33zuli@gmail.com>
2017-03-31 10:09:54 +0200

    Update main script

--- deliverix.sh
+++ deliverix.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-cd /home/phombo/deliverix && rm -f menu.pdf && curl https://restauracie.sme.sk/export/resmenu/4129 >> menu.pdf && lpr menu.pdf && echo `date` >> deliverix.log
-
+cd /home/phombo/deliverix
+echo -n `date` >> deliverix.log
+rm -f menu.pdf && curl https://restauracie.sme.sk/export/resmenu/4129 >> menu.pdf && lpr menu.pdf
+echo "... Success" >> deliverix.log
